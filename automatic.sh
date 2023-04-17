4e256f978da63dfd1bab9592a8b6f00afd726e78
hdaojin <hdaojin@hotmail.com>
2023-04-18 00:00:53 +0800

    modified automatic.sh

--- automatic.sh
+++ automatic.sh
@@ -6,4 +6,4 @@ source $source_dir/.venv/bin/activate  &&  python3 $source_dir/aliy_signin.py
 
 sleep 10
 
-python3 $source_dir/aliy_download.py 
+sudo python3 $source_dir/aliy_download.py  Offline  /usb_disk1/
